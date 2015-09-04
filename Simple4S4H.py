fa9c1659741e126eb708586a05622a05e1a40d41
chenlundeSDN <pourmichael@gmail.com>
2015-09-04 21:13:57 +0200

    mininet topologies

--- Simple4S4H.py
+++ Simple4S4H.py
@@ -4,7 +4,7 @@
 
 from mininet.topo import Topo
 
-class MyTopo( Topo ):
+class Simple4S4H( Topo ):
     "Simple topology example."
 
     def __init__( self ):
@@ -14,15 +14,28 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink( h1, s1 )
+        self.addLink( h2, s2 )
+        self.addLink( h3, s3 )
+        self.addLink( h4, s4 )
+
+        self.addLink( s1, s2 )
+        self.addLink( s2, s3 )
+        self.addLink( s3, s4 )
+        self.addLink( s4, s1 )
+        self.addLink( s1, s3 )
+        self.addLink( s2, s4 )
 
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+topos = { 'mytopo': ( lambda: Simple4S4H() ) }
